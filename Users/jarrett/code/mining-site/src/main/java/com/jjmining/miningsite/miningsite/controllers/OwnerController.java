6f8bfd72c0dd751a689f2a8bb45c83200d0e9391
jarrett brown <jarrettjbrown@gmail.com>
2017-08-15 22:02:28 -0500

    Add object part,add jsoup price scraper, add btc and ztc header

--- Users/jarrett/code/mining-site/src/main/java/com/jjmining/miningsite/miningsite/controllers/OwnerController.java
+++ Users/jarrett/code/mining-site/src/main/java/com/jjmining/miningsite/miningsite/controllers/OwnerController.java
@@ -1,5 +1,6 @@
 package com.jjmining.miningsite.miningsite.controllers;
 
+import com.jjmining.miningsite.miningsite.models.Part;
 import com.jjmining.miningsite.miningsite.models.data.CustomerDao;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -24,7 +25,10 @@ public class OwnerController {
     @RequestMapping(value = "")
     public String index(Model model) throws IOException {
         model.addAttribute("title", "Owner Info");
+        model.addAttribute("btcPrice","$4001");
+        model.addAttribute("ztcPrice","$206");
 
+        /*
         String url = "https://pcpartpicker.com/product/Q9Gj4D/asus-motherboard-z170a";
         Document document = Jsoup.connect(url).get();
 
@@ -33,10 +37,17 @@ public class OwnerController {
 
         Element link = document.select("td.total a").first();
         String moboLink = link.attr("abs:href");
-
+        //
         model.addAttribute("moboName", moboName);
         model.addAttribute("moboPrice", moboPrice);
         model.addAttribute("moboLink",moboLink);
+        */
+        Part mobo = new Part("Motherboard","https://pcpartpicker.com/product/Q9Gj4D/asus-motherboard-z170a");
+        Part cpu = new Part("CPU","https://pcpartpicker.com/product/8gKhP6/intel-pentium-g4560-35ghz-dual-core-processor-bx80677g4560");
+        Part ram = new Part("RAM","https://pcpartpicker.com/product/Z8KhP6/crucial-memory-bls4g4d240fsb");
+        model.addAttribute("mobo",mobo);
+        model.addAttribute("cpu",cpu);
+        model.addAttribute("ram",ram);
 
         return "owner/home";
     }
---
+++ Users/jarrett/code/mining-site/src/main/java/com/jjmining/miningsite/miningsite/models/Part.java
@@ -0,0 +1,99 @@
+package com.jjmining.miningsite.miningsite.models;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.io.IOException;
+
+/**
+ * Created by jarrett on 8/15/2017.
+ */
+
+@Entity
+public class Part {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    private String type;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String price;
+
+    @NotNull
+    private String link;
+
+    @NotNull
+    private String pcUrl;
+
+    public Part(String type, String pcUrl) throws IOException {
+        this.type = type;
+        this.pcUrl = pcUrl;
+        Document document = Jsoup.connect(pcUrl).get();
+        String partName = document.select(".title h1").text();
+        String partPrice = document.select("td.total a").first().text();
+        Element partLink = document.select("td.total a").first();
+        String relHref = partLink.attr("href");
+        String absHref = partLink.attr("abs:href");
+        this.name = partName;
+        this.price = partPrice;
+        this.link = absHref;
+    }
+
+    public Part() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getPcUrl() {
+        return pcUrl;
+    }
+
+    public void setPcUrl(String pcUrl) {
+        this.pcUrl = pcUrl;
+    }
+}
